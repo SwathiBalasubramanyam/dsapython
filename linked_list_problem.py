9e899b85a990d334a4b1494379cb72c36200b1fe
swathi <swathibalasubramanyam@gmail.com>
2024-03-07 14:17:18 -0800

    mid, rev linked list

--- linked_list_problem.py
+++ linked_list_problem.py
@@ -23,4 +23,82 @@ class Solution:
 
         return True if get_max_depth(root) != -1 else False
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# https://leetcode.com/problems/remove-linked-list-elements/
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         
+        dummyNode = ListNode()
+        root = dummyNode
+
+        while head:
+            if head.val != val:
+                root.next = head
+                root = root.next
+            else:
+                root.next = None
+            head = head.next
+        return dummyNode.next
+    
+# https://leetcode.com/problems/reverse-linked-list/
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummyNode = None
+        root = dummyNode
+
+        while head:
+            curr_next = head.next
+
+            head.next = root
+            root = head
+            head = curr_next
+
+        return root
+
+# https://leetcode.com/problems/middle-of-the-linked-list/description/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    import math
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        # slow = fast  = head
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+
+        # return slow
+        
+        # len = 0
+        # curr = head
+        # while curr:
+        #     len += 1
+        #     curr = curr.next
+
+        # mid = math.floor((len/2) + 1)
+        
+        # curr_idx = 1
+        # curr_node = head
+        # while curr_idx != mid:
+        #     curr_node = curr_node.next
+        #     curr_idx += 1
+
+        # return curr_node
+
+        def find_mid(slow, fast):
+            if not fast or not fast.next:
+                return slow
+
+            return find_mid(slow.next, fast.next.next)
+
+        return find_mid(head, head)
+        
+
+            
