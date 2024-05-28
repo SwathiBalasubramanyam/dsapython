9be057b02c4280551934fe916d45bd6d591fb74e
swathi <swathibalasubramanyam@gmail.com>
2024-05-28 00:01:55 -0700

    stacks using queue

--- leetcode_easy.py
+++ leetcode_easy.py
@@ -1131,3 +1131,30 @@ class Solution:
         return self.countNodes(root.left) + 1 + self.countNodes(root.right)
         
         
+# https://leetcode.com/problems/implement-stack-using-queues/
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque([])
+        
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        return self.queue.pop()
+
+    def top(self) -> int:
+        return self.queue[-1]
+
+    def empty(self) -> bool:
+        return not bool(self.queue)
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
