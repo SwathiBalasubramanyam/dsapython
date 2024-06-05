e8e2cbda3679167753b7e0c37e8548cc6c3b45ce
swathi <swathibalasubramanyam@gmail.com>
2024-06-05 12:14:55 -0700

    HP problems

--- headstarter_problems.py
+++ headstarter_problems.py
@@ -34,3 +34,29 @@ def cat_dog(s):
     return cat_cnt == dog_cnt
 
 print(cat_dog("1cat1cadodog"))
+
+def find_id_pair(nums, target):
+    num_to_id = {}
+    for idx, num in enumerate(nums):
+        component = target - num
+
+        if component in num_to_id and num_to_id[component] != idx:
+            return [idx, num_to_id[component]]
+        
+        num_to_id[num] = idx
+        
+
+# Example 1:
+# - Input: nums = [2, 7, 11, 15], target = 9
+# - Output: [0, 1]
+print(find_id_pair([2, 7, 11, 15], 9))
+
+# Example 2:
+# - Input: nums = [3, 2, 4], target = 6
+# - Output: [1, 2]
+print(find_id_pair([3,2,4], 6))
+
+# Example 3:
+# - Input: nums = [3,3], target = 6
+# - Output: [0,1]
+print(find_id_pair([3,3], 6))
