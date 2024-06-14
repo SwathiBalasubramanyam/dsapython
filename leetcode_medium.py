4811c11c3456e491ac4ad9aadba4868c8c19679b
swathi <swathibalasubramanyam@gmail.com>
2024-06-13 17:10:43 -0700

    rotated arr

--- leetcode_medium.py
+++ leetcode_medium.py
@@ -760,4 +760,38 @@ class Solution:
             num = num % k
 
         return res_str
+    
+# https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1287536479/
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+
+            mid = left + (right-left) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # left portion of array
+            if nums[left] <= nums[mid]:
+                if target > nums[mid]:
+                    left = mid + 1
+                elif target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid-1
+
+            # right portion of array
+            else:
+                if target < nums[mid]:
+                    right = mid-1
+                elif target > nums[right]:
+                    right = mid-1
+                else:
+                    left = mid+1
+                
+
+        return -1
+        
 
