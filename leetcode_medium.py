53397a96caec0ec88c53870b78fd22ed1dd9591f
swathi <swathibalasubramanyam@gmail.com>
2024-06-20 17:37:21 -0700

    backtrack - combination sum

--- leetcode_medium.py
+++ leetcode_medium.py
@@ -856,3 +856,30 @@ class Solution:
             r += 1
         return False
 
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        res = []
+        can_len = len(candidates)
+        def dfs(i, curr, total):
+
+            if total == target:
+                res.append(curr.copy())
+                return 
+
+            if total > target or i >= can_len:
+                return
+
+            curr.append(candidates[i])
+            dfs(i, curr, total+candidates[i])
+
+            curr.pop()
+            dfs(i+1, curr, total)
+
+        dfs(0, [], 0)
+        return res
+
+            
+
+
+        
