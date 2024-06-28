9c6ed13f4855481e88522b70d0fa3eee4730d4a8
swathi <swathibalasubramanyam@gmail.com>
2024-06-28 12:56:36 -0700

    LL-hard

--- leetcode_hard.py
+++ leetcode_hard.py
@@ -43,3 +43,45 @@ class Solution:
                 visited.add((new_row, new_col, elims_remaining))
 
         return -1
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                L1 = lists[i]
+                L2 = lists[i+1] if (i+1)<len(lists) else None
+                mergedLists.append(self.mergeList(L1, L2))
+
+            lists = mergedLists
+        return lists[0]
+
+    def mergeList(self, L1, L2):
+        dummy = ListNode()
+        tail = dummy
+
+        while L1 and L2:
+            if L1.val <= L2.val:
+                tail.next = L1
+                L1 = L1.next
+            else:
+                tail.next = L2
+                L2 = L2.next
+            tail = tail.next
+
+        if L1:
+            tail.next = L1
+        if L2:
+            tail.next = L2
+        return dummy.next
+
+        
